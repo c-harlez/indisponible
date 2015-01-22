b29f019cc3fb0bbf07f038c5b46de6c625b9e70d
root <root@vps132881.ovh.net>
2015-01-23 00:58:23 +0100

    + add #! check to show/hide alert

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
+  var hash = document.URL.substr(document.URL.indexOf('#')+1);
 
+  if('!' == hash) {
+    $('#indisponible').show();
+  } else {
+    $('#indisponible').hide();
+  }
+});
