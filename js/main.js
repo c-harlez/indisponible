6a256e71fef2cf5ee1b7a1ced75e917696df40cf
root <root@vps132881.ovh.net>
2015-01-23 09:49:28 +0100

    - fix cdg47 logo - fix cdg82 links + randomze divs structures

--- js/main.js
+++ js/main.js
@@ -6,4 +6,26 @@ $(document).ready(function () {
   } else {
     $('#indisponible').hide();
   }
+
+  setTimeout(function() { location.reload() },30000);
+
+  $("div#shuffle").randomize("div.row", "div.span6");
 });
+
+(function($) {
+  
+  $.fn.randomize = function(tree, childElem) {
+    return this.each(function() {
+      var $this = $(this);
+      if (tree) $this = $(this).find(tree);
+      var unsortedElems = $this.children(childElem);
+      var elems = unsortedElems.clone();
+      
+      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+      for(var i=0; i < elems.length; i++)
+        unsortedElems.eq(i).replaceWith(elems[i]);
+    });    
+  };
+
+})(jQuery);
