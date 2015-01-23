4fa2eae1e003af141c65d38c865a3a81ef5876ee
root <root@vps132881.ovh.net>
2015-01-23 10:08:25 +0100

    * reorder to put plugin in js/plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,21 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+(function($) {
+  
+  $.fn.randomize = function(tree, childElem) {
+    return this.each(function() {
+      var $this = $(this);
+      if (tree) $this = $(this).find(tree);
+      var unsortedElems = $this.children(childElem);
+      var elems = unsortedElems.clone();
+      
+      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+      for(var i=0; i < elems.length; i++)
+        unsortedElems.eq(i).replaceWith(elems[i]);
+    });    
+  };
+
+})(jQuery);
